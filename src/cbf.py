acb5b26c7fd11e8584437f07f21f680060bff5bb
Thomas Wanchai MENIER <wanchai.menier@gmail.com>
2025-07-18 00:48:15 +0900

    feat: mutliple obstacle avoidance for CBF

--- src/cbf.py
+++ src/cbf.py
@@ -5,17 +5,31 @@ from collections import namedtuple
 Obstacle = namedtuple('Obstacle', ['xy', 'r'])
 
 def zeroing_cbf(p: np.ndarray, v_nom: np.ndarray, alpha: float,
-                obstacle: Obstacle, avoid_offset: float):
+                obstacles: list[Obstacle], avoid_offset: float):
+    """
+    Zeroing CBF for robot at position `p` and with velocity `v_nom` originally applied.
+    All obstacles will have the same `alpha` parameter applied
+    """
     if alpha <= 0.:
         raise ValueError("alpha must be > 0.")
 
-    D = obstacle.r + avoid_offset
-
     # CBF solve
+    ## Objective function
     P = matrix(2 * np.identity(2))
     q = matrix(-2 * v_nom)
-    G = matrix((obstacle.xy - p))
-    h = matrix((alpha / 2.) * (np.linalg.norm(p - obstacle.xy) ** 2 - D ** 2))
+
+    ## Constraints
+    def gen_obstacle(o: Obstacle):
+        D = o.r + avoid_offset
+        return (alpha / 2.) * (np.linalg.norm(p - o.xy) ** 2 - D ** 2)
+
+    s = np.array(obstacles[0].xy - p)
+    for i in range(1, len(obstacles)):
+        s = np.append(s, [[0, 1]] - p, axis=0)
+    G = matrix(s)
+    h = matrix([
+        gen_obstacle(o) for o in obstacles
+    ])
 
     return solvers.qp(P, q, G, h, options={'show_progress': False})
 
@@ -26,12 +40,15 @@ if __name__ == '__main__':
     grSimController = Controller()
 
     target = np.array([1, 1])
-    obs = Obstacle(xy=np.array([[0., 0.]]), r=0.1)
+    obs = [
+        Obstacle(xy=np.array([[0., 0.]]), r=0.1),
+        Obstacle(xy=np.array([[0., 1.]]), r=0.1)
+    ]
     def orders(teams_data):
         blue0 = teams_data["blue"][0]
         blue0_pos = blue0.pos
         cmd = target - blue0_pos
-        sol = zeroing_cbf(blue0_pos, cmd, alpha=4, obstacle=obs, avoid_offset=0.05)
+        sol = zeroing_cbf(blue0_pos, cmd, alpha=4, obstacles=obs, avoid_offset=0.05)
         print(f"Offset : {np.linalg.norm(sol['x'] - matrix(cmd)) ** 2}")
         return {"blue": {0: sol["x"]}}
 
