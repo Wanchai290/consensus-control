0cb97b11cd8c8ef030b0e8720cf7583e85df7eba
Thomas Wanchai MENIER <wanchai.menier@gmail.com>
2025-08-05 23:30:52 +0900

    fix: node id of graphs start at 0

--- src/util.py
+++ src/util.py
@@ -3,17 +3,17 @@ import networkx as nx
 def example_graph1():
     # balanced example graph
     G = nx.DiGraph()
-    G.add_nodes_from(range(1, 6))
+    G.add_nodes_from(range(6))
     G.add_edges_from([
-        (1, 4), (4, 3), (3, 2), (2, 1),
-        (2, 6), (6, 5), (5, 2)
+        (0, 3), (3, 2), (2, 1), (1, 0),
+        (1, 5), (5, 4), (4, 1)
     ])
     return G
 
 def three_agents():
     G = nx.DiGraph()
-    G.add_nodes_from(range(1, 3))
+    G.add_nodes_from(range(3))
     G.add_edges_from([
-        (1, 2), (2, 3), (3, 1)
+        (0, 1), (1, 2), (2, 0)
     ])
     return G
